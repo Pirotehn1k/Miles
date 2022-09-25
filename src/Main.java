fa9ff77b4fcdc3e768856c8528650afc8f516cff
Yulian Akshov <AKsh4751@gmail.com>
2022-09-25 20:05:26 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 13200;
+        int miles = 20;
+        int bonus = price / miles;
+        System.out.println(bonus);
+
+    }
+
+}
